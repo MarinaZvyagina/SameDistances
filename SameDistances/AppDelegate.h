652f070c06a44bfea61de7117555135b97dcb210
Марина Звягина <zvyagina.marina@gmail.com>
2017-04-24 01:05:08 +0300

    Initial Commit

---
+++ SameDistances/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SameDistances
+//
+//  Created by Марина Звягина on 24.04.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SameDistances/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SameDistances
+//
+//  Created by Марина Звягина on 24.04.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
