272aad7ea4e7aa6bc8bc41dd92f43e14ffcb47c4
Марина Звягина <zvyagina.marina@gmail.com>
2017-04-25 00:46:06 +0300

    Dynamic allocation of buttons and making Constraints

--- SameDistances/ViewController.m
+++ SameDistances/ViewController.m
@@ -22,49 +22,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    NSUInteger widthOfElement = 20;
-    NSInteger distanceBetweenElements = 20;
+    NSUInteger widthOfElement = self.view.bounds.size.width/(NUMBER*4);   //1;//self.view.bounds.size.width/();//20;
+    NSInteger distanceBetweenElements = self.view.bounds.size.width/(NUMBER*3);// 20;
     NSUInteger distanceFromTop = 50;
+    MASViewAttribute * almostLeftAttribute = self.view.mas_left;
+    MASViewAttribute * almostRightAttribute = self.view.mas_right;
+    MASViewAttribute * leftAttribute;
     
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [self.view addSubview:button];
-     button.backgroundColor = [UIColor greenColor];
-    UIButton *button2 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [self.view addSubview:button2];
-    button2.backgroundColor = [UIColor greenColor];
-    UIButton *button3 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [self.view addSubview:button3];
-    button3.backgroundColor = [UIColor greenColor];
-    UIButton *button4 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [self.view addSubview:button4];
-    button4.backgroundColor = [UIColor greenColor];
+    NSMutableArray<UIView *> *buttons = [NSMutableArray<UIView *> new];
     
-    
-    void (^makeConstraint)(MASConstraintMaker *, MASViewAttribute *, MASViewAttribute *) = ^(MASConstraintMaker *make, MASViewAttribute *leftViewAttribute, MASViewAttribute *rightViewAttribute) {
+    void (^makeConstraint)(MASConstraintMaker *, MASViewAttribute *) = ^(MASConstraintMaker *make, MASViewAttribute *leftViewAttribute) {
         make.top.equalTo(self.view.mas_top).with.offset(distanceFromTop);
         make.left.equalTo(leftViewAttribute).with.offset(distanceBetweenElements);
-        make.right.equalTo(rightViewAttribute).with.offset(-distanceBetweenElements);
     };
-
-    [button mas_makeConstraints:^(MASConstraintMaker *make) {
-        makeConstraint(make, self.view.mas_left, button2.mas_left);
-        make.width.greaterThanOrEqualTo(@(widthOfElement)).with.priorityLow();
-    }];
-
-    [button2 mas_makeConstraints:^(MASConstraintMaker *make) {
-        makeConstraint(make, button.mas_right, button3.mas_left);
-        make.width.equalTo(button.mas_width);
-    }];
-
-    [button3 mas_makeConstraints:^(MASConstraintMaker *make) {
-        makeConstraint(make, button2.mas_right, button4.mas_left);
-        make.width.equalTo(button.mas_width);
-    }];
-
-    [button4 mas_makeConstraints:^(MASConstraintMaker *make) {
-        makeConstraint(make, button3.mas_right, self.view.mas_right);
-        make.width.equalTo(button.mas_width);
-    }];
+    
+    for (NSUInteger i=0; i< NUMBER; i++) {
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+        button.backgroundColor = [UIColor greenColor];
+        [self.view addSubview:button];
+        [buttons addObject:button];
+        
+        leftAttribute = i == 0 ? almostLeftAttribute : buttons[i-1].mas_right;
+        
+        [button mas_makeConstraints:^(MASConstraintMaker *make) {
+            makeConstraint(make, leftAttribute);
+            if (i==0)
+                make.width.greaterThanOrEqualTo(@(widthOfElement)).with.priorityLow();
+            else
+                make.width.equalTo(buttons[0].mas_width);
+            if (i == NUMBER-1)
+                make.right.equalTo(almostRightAttribute).with.offset(-distanceBetweenElements);
+        }];
+    }
 }
 
 
